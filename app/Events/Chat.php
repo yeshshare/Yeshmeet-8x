2945da931050bc139a8bef7fb18f97de611baca5
Ruan Santana <ruan@yesh.com.br>
2022-02-10 16:48:36 -0300

    Second

---
+++ app/Events/Chat.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Chat implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $user;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        //
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+    public function broadcastWith()
+    {
+        //dd($this->user);
+        return ['id' => $this->user->id];
+    }
+}
---
+++ resources/js/components/chat.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.');
+
+            Echo.channel('chat')
+
+            /*Echo.channel("'recebida" + result + "'")*/
+            .listen('Chat',(e) => {
+               //this.msgs.push(e);
+               //this.$nextTick(() => this.scrollToEnd());
+                console.log('Acessou');
+                console.log(e);
+               //alert(e);
+            });
+        }
+    }
+</script>
